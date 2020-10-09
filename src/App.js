05c6de78ea503f3af8ca3e798a39c15af45acbd6
Matthew Serwer <Matthew.Serwer@gmail.com>
2020-10-09 12:01:11 -0400

    successfully got library of dead mortys from the api. wrote code for the Morty component inside app.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
+import Characters from './components/Character';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-
+const [mortys, setMortys] = useState([])
+const [currentMortyId, setCurrentMortyId] = useState(null)
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  useEffect(() => {
+    axios.get('https://rickandmortyapi.com/api/character/?name=morty&status=dead')
+    .then(res => {
+      setMortys(res.data)
+    })
+    .catch(err => {
+      debugger
+    })
+  }, [])
+
+
+
 
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <h1 className="Header">Dead Morty's (Documented)</h1>
     </div>
   );
 }
