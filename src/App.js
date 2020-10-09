345a43ad5baef925ab3fdf5d8f25bf0e46187acc
Matthew Serwer <Matthew.Serwer@gmail.com>
2020-10-09 12:20:13 -0400

    got our list of mortys up. time to work on Character.js

--- src/App.js
+++ src/App.js
@@ -1,34 +1,55 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import './App.css';
-import Characters from './components/Character';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
+import Character from "./components/Character";
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-const [mortys, setMortys] = useState([])
-const [currentMortyId, setCurrentMortyId] = useState(null)
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
+  const [mortys, setMortys] = useState([]);
+  const [currentMortyId, setCurrentMortyId] = useState(null);
+  // Fetch characters from the API in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
   useEffect(() => {
-    axios.get('https://rickandmortyapi.com/api/character/?name=morty&status=dead')
-    .then(res => {
-      setMortys(res.data)
-    })
-    .catch(err => {
-      debugger
-    })
-  }, [])
+    axios
+      .get("https://rickandmortyapi.com/api/character/?name=morty&status=dead")
+      .then((res) => {
+        setMortys(res.data.results);
+      })
+      .catch((err) => {
+        debugger;
+      });
+  }, []);
 
+  console.log(mortys);
 
+  const openDetails = (id) => {
+    setCurrentMortyId(id);
+  };
 
+  const closeDetails = () => {
+    setCurrentMortyId(null);
+  };
 
-  return (
-    <div className="App">
-      <h1 className="Header">Dead Morty's (Documented)</h1>
+  const Morty = (props) => (
+    <div className='morty'>
+      {props.info.name}
+      <button onClick={() => openDetails(props.info.id)}>Character Info</button>
     </div>
   );
-}
+
+  return (
+    <div className='App'>
+      <h1 className='Header'>Dead Morty's (Documented)</h1>
+      {mortys.map((mt) => {
+        return <Morty key={mt.id} info={mt} />;
+      })}
+      {
+        currentMortyId && <Character mortyId={currentMortyId} />
+      }
+    </div>
+  );
+};
 
 export default App;
